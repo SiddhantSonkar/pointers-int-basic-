b10b50c5dcbf036d7034eeae9dcd80dd35827c41
SiddhantSonkar <124968287+SiddhantSonkar@users.noreply.github.com>
2023-04-11 09:11:48 +0530

    Add files via upload

---
+++ pointers.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a=4;
+	int *ap=&a;
+	printf("a= %d",a);
+		printf("\n&a = %d",&a);
+		printf("\nap = %d",ap);
+		printf("\n&ap = %d",&ap);
+		printf("\n*ap = %d",*ap);
+	
+	
+	
+	
+}
